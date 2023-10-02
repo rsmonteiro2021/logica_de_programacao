f6a3808d62ce566b303b695fb26174d75604ef1b
Roberto Monteiro <monteiroengenharia@gmail.com>
2023-10-01 22:21:18 -0300

    soma_impares
    Soma os números impares compreendidos dentro de um intervalo dado pelo usuário..

---
+++ liguagem_C/soma_impares.c
@@ -0,0 +1,60 @@
+/******************************************************************************
+
+O programa permite somar os números ímpares compreendidos dentro de um intervalo
+fechado.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void ler_texto(char *buffer, int length){
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
+
+void limpar_entrada(){
+    char c;
+    while((c=getchar()) != 'n'&& c != EOF){}
+}
+
+int main(){
+    int x, y, soma;
+    soma = 0;
+    
+    printf("Digite um numero inteiro: \n");
+    scanf("%d", &x);
+    printf("Digite outro numero: \n");
+    scanf("%d", &y);
+    
+    if (x < y){
+        while (x <= y){
+            if (x % 2 == 0){
+                x++;
+                soma = soma + x;
+                x++;
+            }
+            else{
+                soma = soma + x;
+                x++;
+            }
+        }
+        printf("SOMA = %d", soma);
+    }
+    else{
+        while (y <= x){
+            if (y % 2 == 0){
+                y++;
+                soma = soma + y;
+                y++;
+            }
+            else{
+                soma = soma + y;
+                y++;
+            }
+        }
+        printf("SOMA = %d", soma);
+    }
+
+    return 0;
+}
